0ac9e8988bb8bc8655d1258bb60873e6e0e8fb63
vinoth-kct <vinothj086@gmail.com>
2025-04-08 16:02:00 +0000

    feedback model done

--- springapp/src/main/java/com/examly/springapp/model/Feedback.java
+++ springapp/src/main/java/com/examly/springapp/model/Feedback.java
@@ -1,5 +1,33 @@
 package com.examly.springapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
 public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long feedbackId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    User user;
+    String message;
+    int rating;
     
 }
