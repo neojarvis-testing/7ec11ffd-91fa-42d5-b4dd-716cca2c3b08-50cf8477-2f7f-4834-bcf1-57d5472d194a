11c3f46c619ae7afc470cdfc91041d9df0060b78
Nakuld23 <nakulachar9@gmail.com>
2025-04-09 03:49:46 +0000

    Vehicle Maintainenance Backend

--- springapp/src/main/java/com/examly/springapp/repository/VehicleServiceRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/VehicleServiceRepo.java
@@ -1,5 +1,18 @@
 package com.examly.springapp.repository;
 
-public class VehicleServiceRepo {
+import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.examly.springapp.model.VehicleMaintenance;
+
+@Repository
+public interface VehicleServiceRepo extends JpaRepository<VehicleMaintenance,Long>{
+
+    @Query("select v from VehicleMaintenance v where v.serviceName like :serviceName")
+    public List<VehicleMaintenance> findByServiceName(String serviceName);
     
 }
