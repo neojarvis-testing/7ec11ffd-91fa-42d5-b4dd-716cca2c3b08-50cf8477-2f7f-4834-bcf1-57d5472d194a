e592c55962d653b1e7855342a6eaf22a81583c73
joshideep256 <joshideep256@gmail.com>
2025-04-09 09:35:49 +0000

    09-05-2025 Created required user related files

--- springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
@@ -1,5 +1,69 @@
 package com.examly.springapp.service;
 
-public class UserServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.User;
+import com.examly.springapp.repository.UserRepo;
+
+import jakarta.persistence.EntityNotFoundException;
+
+
+@Service
+
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public User createUser(User user) {
+        return userRepo.save(user);
+    }
+
+    //review again
+    @Override
+    public User loadUserByUsername(String userName) {
+        return userRepo.findByUserName(userName);
+
+
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+       return userRepo.findAll();
+    }
+
+    @Override
+    public User getByUserId(int userId) {
+        Optional<User> opt=userRepo.findById(userId);
+        if(opt.isEmpty()){
+            throw new EntityNotFoundException("UserId not found");
+        }
+        return opt.get();
+    }
+
+    @Override
+    public void deleteUser(int userId) {
+        userRepo.deleteById(userId);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        Optional<User> opt=userRepo.findById(user.getUserId());
+        if(opt.isEmpty()){
+            throw new EntityNotFoundException("UserId not found");
+        }
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserByName(String name) {
+        return userRepo.getUserByName(name);
+    }
+
     
 }
