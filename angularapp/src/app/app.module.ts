c63594cf165c6b4519c805ff453b94176f527e68
vinoth-kct <vinothj086@gmail.com>
2025-04-09 08:45:23 +0000

    demo commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,14 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-
-
+import {HttpClientModule} from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -57,7 +49,8 @@ import { UserviewfeedbackComponent } from './userviewfeedback/userviewfeedback.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularapp/src/app/models/feedback.model.ts
+++ angularapp/src/app/models/feedback.model.ts
@@ -1,2 +1,9 @@
+import { User } from "./user.model";
+
 export interface Feedback {
+
+    feedbackId?:number;
+    user:User;
+    message:string;
+    rating:number;
 }
