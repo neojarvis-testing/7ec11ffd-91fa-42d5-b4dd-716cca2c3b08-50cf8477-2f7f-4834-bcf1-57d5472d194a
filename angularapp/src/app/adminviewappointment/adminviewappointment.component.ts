7aaee3645f8698665546860a97feda61aa3a95f2
Priyadharshini-M-914 <m.priyadharshinicse2020@gmail.com>
2025-04-12 03:45:45 +0000

    apr 12-9.15

--- angularapp/src/app/adminviewappointment/adminviewappointment.component.ts
+++ angularapp/src/app/adminviewappointment/adminviewappointment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppointmentService } from '../services/appointment.service';
 import { Appointment } from '../models/appointment.model';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-adminviewappointment',
@@ -9,27 +10,30 @@ import { Appointment } from '../models/appointment.model';
 })
 export class AdminviewappointmentComponent implements OnInit {
 
-  appointments:Appointment[]=[];
+  appointments: Appointment[] = [];
+
   showDeletePopup: boolean = false;
   appointmentId: number | null = null;
+  inp: string = "";
 
-  constructor(private appointmentService:AppointmentService) { }
+  constructor(private appointmentService: AppointmentService) { }
 
   ngOnInit(): void {
     this.loadAppointments();
   }
 
-  loadAppointments(){
-    this.appointmentService.getAppointments().subscribe(data=>{
-      this.appointments=data;
-    })
+  loadAppointments() {
+    this.appointmentService.getAppointments().subscribe(data => {
+      this.appointments = data;
+    });
   }
 
-  deleteAppointment(appointmentId:number){
-    this.appointmentService.deleteAppointment(appointmentId).subscribe(data1=>{
+  deleteAppointment(appointmentId: number) {
+    this.appointmentService.deleteAppointment(appointmentId).subscribe(data1 => {
       this.showDeletePopup = false;
+      this.appointmentId = null;
       this.loadAppointments();
-    })
+    });
   }
 
   showPopup(id: number) {
@@ -37,4 +41,15 @@ export class AdminviewappointmentComponent implements OnInit {
     this.showDeletePopup = true;
   }
 
+  hidePopup() {
+    this.showDeletePopup = false;
+    this.appointmentId = null;
+  }
+
+  searchData() {
+    this.appointmentService.getAppointments().subscribe(data => {
+      this.appointments = data;
+      this.appointments = this.appointments.filter(b => JSON.stringify(b).toLowerCase().includes(this.inp.toLowerCase()));
+    });
+  }
 }
--- angularapp/src/app/userviewappointment/userviewappointment.component.ts
+++ angularapp/src/app/userviewappointment/userviewappointment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppointmentService } from '../services/appointment.service';
 import { Appointment } from '../models/appointment.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-userviewappointment',
@@ -11,17 +12,21 @@ export class UserviewappointmentComponent implements OnInit {
 
   appointments:Appointment[]=[];
   selectedStatus:string="All";
+  inp:string="";
+  userId:number|null;
 
-  constructor(private appointmentService:AppointmentService) { }
+  constructor(private appointmentService:AppointmentService,private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.userId=parseInt(this.authService.getUserId());
+    this.getAppoinments();
   }
 
-  // getAppoinments(){
-  //   this.appointmentService.getAppointmentsByUser(userId).subscribe(data=>{
-  //     this.appointments=data;
-  //   })
-  // }
+  getAppoinments(){
+    this.appointmentService.getAppointmentsByUser(this.userId).subscribe(data=>{
+      this.appointments=data;
+    })
+  }
 
   // getAppoinmentsByFilter(){
   //   if(this.selectedStatus=='All'){
@@ -34,4 +39,21 @@ export class UserviewappointmentComponent implements OnInit {
   //     })
   // }
 
+  getAppoinmentsByFilter(){
+      if(this.selectedStatus=='All'){
+         this.appointments;
+      }
+      else
+      // this.appointmentService.getAppointmentsByUser(userId).subscribe(data=>{
+      //    this.appointments=data;
+         this.appointments=this.appointments.filter(a=>a.status == this.selectedStatus);
+        // })
+    }
+
+    searchData(){
+      // this.appointmentService.getAppointmentsByUser(userId).subscribe(data=>{
+      //    this.appointments=data;
+      this.appointments=this.appointments.filter(b=>JSON.stringify(b).toLowerCase().includes(this.inp.toLowerCase()));
+      // })
+    }
 }
